e0d8105a442f34bcb396008d568a11d20e8824ee
Zelda Ogiamien <zel0789@yahoo.com>
2023-02-28 17:46:41 -0500

    created components: navbar and ProductCard, created features folder, created productList and singleProduct folders:created started components and slices

---
+++ src/features/product/productList/ProductList.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductCard from "../../../components/productCard/ProductCard.jsx";
+
+// const ProductList = () => {
+//     const dispatch = useDispatch();
+//     const products = useSelector((state) => state.products);
+//     const status = useSelector((state) => state.products.status);
+//     const error = useSelector((state) => state.products.error)
+// }
