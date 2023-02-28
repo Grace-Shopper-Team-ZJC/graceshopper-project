f4e0e50821ea6fa41a4a22ee9e586ed357110a2a
Caroline Boyle <carolinerboyle@gmail.com>
2023-02-28 14:54:19 -0500

    initialized models and seeding database(incomplete)

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,24 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
+const Guest = require("./models/Guest");
+const Order = require("./models/Order");
+const Product = require("./models/Product");
+const Cart = require("./models/Cart");
 
 //associations could go here!
+User.hasMany(Order);
+Order.belongsTo(User);
 
 module.exports = {
   db,
   models: {
     User,
+    Guest,
+    Order,
+    Product,
+    Cart,
   },
-}
+};
