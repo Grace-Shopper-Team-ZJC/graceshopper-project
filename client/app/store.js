1e6c00525944c9d18cd4b0507beb9cb1bb83f9be
Caroline Boyle <carolinerboyle@gmail.com>
2023-03-02 15:55:21 -0500

    functional login and sign up with database

--- client/app/store.js
+++ client/app/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import authReducer from "../features/auth/authSlice";
-import userReducer from "../features/user/userSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer, user: userReducer },
+  reducer: { auth: authReducer },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
 export * from "../features/auth/authSlice";
-export * from "../features/user/userSlice";
--- client/features/auth/AuthForm.js
+++ client/features/auth/AuthForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { authenticate, createAccount } from "../../app/store";
+import { authenticate } from "../../app/store";
 
 /**
   The AuthForm component can be used for Login or Sign Up.
@@ -20,58 +20,26 @@ const AuthForm = ({ name, displayName }) => {
     dispatch(authenticate({ username, password, method: formName }));
   };
 
-  const handleAccountCreation = (formData) => {
-    dispatch(createAccount(formData));
-  };
-
   return (
     <div>
-      {displayName === "Sign Up" ? (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            const username = e.target.username.value;
-            const password = e.target.password.value;
-            handleAccountCreation({ username, password });
-          }}
-        >
-          <div>
-            <label htmlFor="username">
-              <small>Username</small>
-            </label>
-            <input name="username" type="text" />
-          </div>
-          <div>
-            <label htmlFor="password">
-              <small>Password</small>
-            </label>
-            <input name="password" type="password" />
-          </div>
-          <div>
-            <button type="submit">{displayName}</button>
-          </div>
-          {error && <div> {error} </div>}
-        </form>
-      ) : (
-        <form onSubmit={handleSubmit} name={name}>
-          <div>
-            <label htmlFor="username">
-              <small>Username</small>
-            </label>
-            <input name="username" type="text" />
-          </div>
-          <div>
-            <label htmlFor="password">
-              <small>Password</small>
-            </label>
-            <input name="password" type="password" />
-          </div>
-          <div>
-            <button type="submit">{displayName}</button>
-          </div>
-          {error && <div> {error} </div>}
-        </form>
-      )}
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="username">
+            <small>Username</small>
+          </label>
+          <input name="username" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">{displayName}</button>
+        </div>
+        {error && <div> {error} </div>}
+      </form>
     </div>
   );
 };
