9a3f9e82c8b0085b74fd5dacc4e2995fab3f6d7f
Zelda Ogiamien <zel0789@yahoo.com>
2023-03-06 14:58:11 -0500

    product list almost finalized

--- client/features/productList/ProductList.js
+++ client/features/productList/ProductList.js
@@ -1,10 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import ProductCard from "../../../components/productCard/ProductCard.jsx";
+import ProductCard from "../../../components/productCard/ProductCard.js";
 
-// const ProductList = () => {
-//     const dispatch = useDispatch();
-//     const products = useSelector((state) => state.products);
-//     const status = useSelector((state) => state.products.status);
-//     const error = useSelector((state) => state.products.error)
-// }
+const ProductList = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
+  const status = useSelector((state) => state.products.status);
+  const error = useSelector((state) => state.products.error);
+};
+
+useEffect(() => {
+  if (status === "idle") {
+    dispatch(fetchProducts());
+  }
+}, [status, dispatch]);
+
+if (status === "loading") {
+  return <div>Loading...</div>;
+}
+
+if (status === "failed") {
+  return <div>{error}</div>;
+}
+
+return (
+  <div>
+    {products.map((product) => (
+      <ProductCard key={product.id} product={product} />
+    ))}
+  </div>
+);
+
+export default ProductList;
